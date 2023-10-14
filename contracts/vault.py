0506a84f15bddbd10025825b665150634602aa0d
Djason Gadiou <86433099+Magicred-1@users.noreply.github.com>
2023-10-14 21:25:23 +0200

    SmartPy is OKAYYYYYY

--- contracts/vault.py
+++ contracts/vault.py
@@ -1,111 +1,91 @@
-# Project name: TBD
-# Made by: Snails Team (Djason G., Elia, Yacine A., Yassine O., Hamza M.)
-
 import smartpy as sp
 
-# A vault is a contract that holds a balance of tez that can deposit, transfer, getTransactions
-# We store user balance, phone number and wallet address
+# Main contract
+@sp.module
 def main():
     class Vault(sp.Contract):
-        def __init__(self, initial_balance = 200000):
-            self.init(
-                balance = initial_balance, 
-                transactions = sp.big_map(
-                        tvalue = sp.TRecord(phone = sp.TNat, wallet = sp.TAddress, amount = sp.TNat),
-                )
-
-        # Deposit tez into the vault
+        def __init__(self):
+            self.data.balances = {}
+            self.data.profile = {}
+            self.data.transactions = {}
+    
+        # Deposit function
         @sp.entry_point
-        def deposit(self):
-            sp.verify(sp.amount > 0 and sp.amount <= )
-            self.data.balance += sp.amount
-
-
-        # Transfer tez from the vault to a wallet
+        def deposit(self, params):
+            # Initialize the balance if it doesn't exist
+            if not self.data.balances.contains(sp.sender):
+                self.data.balances[sp.sender] = sp.amount
+                self.data.profile[sp.sender] = params.phone_number
+            else:
+                # Update the balance if it already exists
+                self.data.balances[sp.sender] += sp.amount
+                self.data.profile[sp.sender] = params.phone_number
+    
+        # Transfer function
         @sp.entry_point
         def transfer(self, params):
-            sp.verify(self.data.balance >= params.amount)
-            sp.send(params.wallet, params.amount)
-            self.data.balance -= params.amount
-
-        # Get transactions from the vault
+            assert self.data.balances.contains(sp.sender), "Sender not in balances"
+            assert self.data.balances[sp.sender] >= sp.amount, "Not enough balance"
+            self.data.balances[sp.sender] -= sp.amount
+            self.data.transactions[sp.sender] = sp.amount
+            if self.data.balances.contains(params.to):
+                self.data.balances[params.to] += sp.amount
+                self.data.profile[params.to] = params.phone_number
+            else:
+                self.data.balances[sp.sender] = sp.amount
+                self.data.transactions[sp.sender] = sp.amount
+    
+        # Get transactions function
         @sp.entry_point
-        def getTransactions(self, params):
-            sp.result(self.data.transactions[params.phone])
+        def getTransactions(self):
+            assert self.data.transactions.contains(sp.sender), "Sender not in transactions"
 
-        # Get balance from the vault
+        @sp.entry_point
+        def getProfile(self):
+            assert self.data.profile.contains(sp.sender), "Sender not in profile"
+
         @sp.entry_point
         def getBalance(self):
-            sp.result(self.data.balance)
+            assert self.data.balances.contains(sp.sender), "Sender not in balances"
+        
+        @sp.entry_point
+        def getTransactionsByAddress(self):
+            assert self.data.transactions.contains(sp.sender), "Sender not in transactions"
+# Tests
+@sp.add_test(name = "Vault test")
+def test():
+    scenario = sp.test_scenario(main)
+    scenario.h1("Vault test")
 
-if "templates" not in __name__:
-    @sp.add_test(name = "User Simulation")
-    def test():
-        scenario = sp.test_scenario()
-        scenario.h1("User Simulation")
+    # Initializing accounts
+    admin = sp.test_account("Admin")
+    user1 = sp.test_account("User1")
+    user2 = sp.test_account("User2")
 
-        # Create a vault contract
-        vault = Vault()
-        scenario += vault
+    # Initializing contract
+    c1 = main.Vault()
+    scenario += c1
 
-        # Deposit 10 tez into the vault
-        scenario.h2("Deposit 10 tez into the vault")
-        scenario += vault.deposit().run(amount = sp.tez(10))
+    # Deposit
+    scenario.h2("Deposit")
+    c1.deposit(phone_number = 33694033344).run(sender = user1, amount = sp.tez(1))
 
-        # Transfer 5 tez from the vault to a wallet
-        scenario.h2("Transfer 5 tez from the vault to a wallet")
-        scenario += vault.transfer(wallet = sp.address("tz1..."), amount = sp.tez(5)).run(sender = sp.address("tz1..."))
+    # Transfer
+    scenario.h2("Transfer")
+    c1.transfer(phone_number = 33694033344, to = user2.address).run(sender = user1, amount = sp.tez(1))
 
-        # Get transactions from the vault
-        scenario.h2("Get transactions from the vault")
-        scenario += vault.getTransactions(phone = 33670210556).run(sender = sp.address("tz1..."))
+    # Get transactions
+    scenario.h2("Get transactions")
+    c1.getTransactions().run(sender = user1.address)
 
-        # Get balance from the vault
-        scenario.h2("Get balance from the vault")
-        scenario += vault.data.balance
+    # Get profile
+    scenario.h2("Get profile")
+    c1.getProfile().run(sender = user1.address)
 
-        # Get transactions from the vault
-        scenario.h2("Get transactions from the vault")
-        scenario += vault.data.transactions
+    # Get balance
+    scenario.h2("Get balance")
+    c1.getBalance().run(sender = user1.address)
 
-        # Get balance from the vault
-        scenario.h2("Get balance from the vault")
-        scenario += vault.data.balance
-
-        # Get transactions from the vault
-        scenario.h2("Get transactions from the vault")
-        scenario += vault.data.transactions
-
-        # Get balance from the vault
-        scenario.h2("Get balance from the vault")
-        scenario += vault.data.balance
-
-        # Get transactions from the vault
-        scenario.h2("Get transactions from the vault")
-        scenario += vault.data.transactions
-
-        # Get balance from the vault
-        scenario.h2("Get balance from the vault")
-        scenario += vault.data.balance
-
-        # Get transactions from the vault
-        scenario.h2("Get transactions from the vault")
-        scenario += vault.data.transactions
-
-        # Get balance from the vault
-        scenario.h2("Get balance from the vault")
-        scenario += vault.data.balance
-
-        # Get transactions from the vault
-        scenario.h2("Get transactions from the vault")
-        scenario += vault.data.transactions
-
-        # Get balance from the vault
-        scenario.h2("Get balance from the vault")
-        scenario += vault.data.balance
-
-        # Get transactions from the vault
-        scenario.h2("Get transactions from the vault")
-        scenario += vault.data.transactions
-
-# Path: contracts/phone.py
+    # Get transactions by number
+    scenario.h2("Get transactions by Address")
+    c1.getTransactionsByAddress().run(sender = user1.address)
