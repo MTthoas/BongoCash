72fe15cab2a448b069634dfdc48b40b40169f18d
Djason Gadiou <86433099+Magicred-1@users.noreply.github.com>
2023-10-14 14:49:29 +0200

    API

---
+++ api/server.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import cors from 'cors';
+import { router } from './routes';
+import { config } from 'dotenv';
+
+config();
+
+const app = express();
+const port = process.env.PORT || 4530;
+
+app.use(cors(), express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.use('/api', router);
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
